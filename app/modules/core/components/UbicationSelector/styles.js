0846764d8f50344e956662c7e896ee19b2e964bf
juanfmoyano <juanfri.m@hotmail.com>
2020-10-22 15:58:16 -0300

    ubicationSelector on Filters home

--- app/modules/core/components/UbicationSelector/styles.js
+++ app/modules/core/components/UbicationSelector/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   buttonContainer: {
     position: 'absolute',
-    bottom: '5%',
+    bottom: '30%',
     width: '100%',
     zIndex: 3,
     flex: 1,
