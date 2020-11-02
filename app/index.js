327ca366089407fdcb5a52e84c0a1f5e0f99dd7e
Juan Olmedo <juuan.olmedo@hotmail.com>
2020-11-02 11:58:40 -0300

    App icon and name changed

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import React from 'react';
+import React, { useEffect } from 'react';
 import reducer from '@store/reducer';
 import sagas from '@store/sagas';
 import configureStore from '@core/store';
@@ -7,19 +7,39 @@ import { NavigationContainer } from '@react-navigation/native';
 import LoginNavigator from '@login/LoginNavigator';
 import { navigationRef } from '@core/utils/navigation';
 import { StatusBar } from 'react-native';
+import RNBootSplash from 'react-native-bootsplash';
 
 const store = configureStore({
   reducer,
   sagas
 });
 
-const App = () => (
-  <Provider store={store}>
-    <StatusBar hidden />
-    <NavigationContainer ref={navigationRef}>
-      <LoginNavigator />
-    </NavigationContainer>
-  </Provider>
-);
+const App = () => {
+  const init = async () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve();
+      }, 3000);
+    });
+  };
+  useEffect(() => {
+    init()
+      .then(() => {
+        RNBootSplash.hide({ duration: 250 });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <StatusBar hidden />
+      <NavigationContainer ref={navigationRef}>
+        <LoginNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+};
 
 export default App;
